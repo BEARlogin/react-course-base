4ec2d7d750b8976945c7f0a7ef0520244cb589c2
a.lugovskoy@geoscan.aero <bearlogin@mail.ru>
2024-12-12 12:49:43 +0300

    add table component

--- js/app.js
+++ js/app.js
@@ -3,40 +3,39 @@ import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// div > h1 | ul > li h2 li h2
-
-// root.render(
-//   React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "Hello from React div"),
-//     React.createElement(
-//       "ul",
-//       {},
-//       React.createElement(
-//         "li",
-//         {},
-//         React.createElement("h2", {}, "Hello from React li 1")
-//       ),
-//       React.createElement(
-//         "li",
-//         {},
-//         React.createElement("h2", {}, "Hello from React li 2")
-//       )
-//     )
-//   )
-// );
+function GridComponent() {
+  return (
+    <table className="table table-condensed">
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>zjTm0@example.com</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+          <td>zjTm0@example.com</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+          <td>zjTm0@example.com</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
 
 root.render(
-  <div>
-    <h1>Hello from React div</h1>
-    <ul>
-      <li>
-        <h2>Hello from React li 1</h2>
-      </li>
-      <li>
-        <h2>Hello from React li 2</h2>
-      </li>
-    </ul>
-  </div>
+  <React.StrictMode>
+    <GridComponent />
+  </React.StrictMode>
 );
