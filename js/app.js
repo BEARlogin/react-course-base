afb718214049d15dd4b56ef1695857cd528f238f
a.lugovskoy@geoscan.aero <bearlogin@mail.ru>
2024-12-12 12:27:28 +0300

    add jsx support

--- js/app.js
+++ js/app.js
@@ -2,24 +2,38 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // div > h1 | ul > li h2 li h2
 
+// root.render(
+//   React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hello from React div"),
+//     React.createElement(
+//       "ul",
+//       {},
+//       React.createElement(
+//         "li",
+//         {},
+//         React.createElement("h2", {}, "Hello from React li 1")
+//       ),
+//       React.createElement(
+//         "li",
+//         {},
+//         React.createElement("h2", {}, "Hello from React li 2")
+//       )
+//     )
+//   )
+// );
+
 root.render(
-  React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Hello from React div"),
-    React.createElement(
-      "ul",
-      {},
-      React.createElement(
-        "li",
-        {},
-        React.createElement("h2", {}, "Hello from React li 1")
-      ),
-      React.createElement(
-        "li",
-        {},
-        React.createElement("h2", {}, "Hello from React li 2")
-      )
-    )
-  )
+  <div>
+    <h1>Hello from React div</h1>
+    <ul>
+      <li>
+        <h2>Hello from React li 1</h2>
+      </li>
+      <li>
+        <h2>Hello from React li 2</h2>
+      </li>
+    </ul>
+  </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,23 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  devtool: 'source-map',
   entry: {
     app: "./js/app.js",
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-react"]
+                }
+            }
+        }
+    ]
   },
   output: {
     path: path.join(__dirname, "build"),
