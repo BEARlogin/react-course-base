df9f566dad6ba91cafbd40ad09022a04bf641651
a.lugovskoy@geoscan.aero <bearlogin@mail.ru>
2024-12-12 12:46:35 +0300

    add style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,31 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: {
     app: "./js/app.js",
   },
   module: {
     rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-react"]
-                }
-            }
-        }
-    ]
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg|gif)$/i,
+        use: ["url-loader?limit=100000"],
+      },
+    ],
   },
   output: {
     path: path.join(__dirname, "build"),
